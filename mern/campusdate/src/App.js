8878936a66ebb6dff56bedb10066faa295a3cc6f
d-sachi <deepthisachi@yahoo.com>
2022-03-18 15:08:51 -0400

    Bootstrap

--- mern/campusdate/src/App.js
+++ mern/campusdate/src/App.js
@@ -2,16 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 
 import React, { Component } from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Link } from "react-router-dom";
+
+
 
 class App extends Component {
   render() {
     return (
       <div>"Welcome to the CampusDate prototypes!"
-      <nav><Link to="/login">Login</Link> {" "}
-      <Link to="/profile">Profile</Link> {" "}
-      <Link to="/editprofile">Edit Profile</Link></nav>
+      <nav class="navbar navbar-expand-lg">
+        <Link class="nav-link" to="/login">Login</Link> {" "}
+        <Link class="nav-link" to="/profile">Profile</Link> {" "}
+        <Link class="nav-link" to="/editprofile">Edit Profile</Link></nav>
       </div> 
     );
   }

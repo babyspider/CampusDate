0901dd0cadaeee6156d42525ffdc7346e0a68724
cindy kwok <kwokc3@rpi.edu>
2022-03-21 20:52:31 -0400

    new copy, nav bar at bottom

--- chat.js
+++ chat.js
@@ -6,44 +6,49 @@ import {
   ScrollView,
   Pressable,
   Alert,
+  Dimensions,
+  Image,
 } from 'react-native';
 import Constants from 'expo-constants';
 
+const screenWidth = Dimensions.get('screen').width;
+const screenHeight = Dimensions.get('screen').height;
+
 export default function App() {
   return (
-    <View style={styles.background}>
-     <ScrollView>
-      <Text style={styles.emailBoxes}>
-      filleremail@emailhost.com
-      </Text>
-      </ScrollView>
-          <View style={{ flexDirection:"row", marginLeft: 0, marginRight:"auto" }}>
-          <Pressable
-            style = {styles.menuButtons}
-            onPress={() => Alert.alert('Setting Menu Button')}>
-              <Image source={require('./assets/settingIcon.png')} style = {styles.iconButtons} />
-          </Pressable>
-          <Pressable
-            style = {styles.menuButtons}
-            onPress={() => Alert.alert('Profile Menu Button')}>
-              <Image source={require('./assets/profileIcon.png')} style = {styles.iconButtons} />
-          </Pressable>
-          <Pressable
-            style = {styles.menuButtons}
-            onPress={() => Alert.alert('Chat Menu Button')}>
-              <Image source={require('./assets/chatIcon.png')} style = {styles.iconButtons} />
-          </Pressable> 
-        </View>
+    <View style={{backgroundColor:'#FECCC4', width: screenWidth, height: screenHeight }}>
+
+      <View style = {{height: screenHeight/1.6}}>  
+        <ScrollView>
+          <Text style={styles.emailBoxes}>
+            filleremail@emailhost.com
+          </Text>
+        </ScrollView>
+      </View>
+
+      <View style={{ flexDirection:"row", marginLeft: 0, marginRight:"auto" }}>
+        <Pressable
+          style = {styles.menuButtons}
+          onPress={() => Alert.alert('Setting Menu Button')}>
+          <Image source={require('./assets/settingIcon.png')} style = {styles.iconButtons} />
+        </Pressable>
+        <Pressable
+          style = {styles.menuButtons}
+          onPress={() => Alert.alert('Profile Menu Button')}>
+          <Image source={require('./assets/profileIcon.png')} style = {styles.iconButtons} />
+        </Pressable>
+        <Pressable
+          style = {styles.menuButtons}
+          onPress={() => Alert.alert('Chat Menu Button')}>
+          <Image source={require('./assets/chatIcon.png')} style = {styles.iconButtons} />
+        </Pressable> 
+      </View>
+      
     </View>
 
   );
 }
 const styles = StyleSheet.create({
-  background: {
-    flex: 1,
-    paddingTop: 25,
-    backgroundColor: 'pink',
-  },
   emailBoxes: {
     padding: 20,
     fontSize: 20,

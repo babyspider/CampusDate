54637405ee536ce47a1c33fc716aa58dde327412
SagetheCapricorn <43279603+SagetheCapricorn@users.noreply.github.com>
2022-03-22 21:45:12 -0400

    trying to implement mongoose

--- mern/server/index.js
+++ mern/server/index.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(cors());
 app.use('/users', userRoute)
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 const server = app.listen(port, () => {
     console.log('Connected to port ' + port)
 })

3d0c68c7f0dc21317cfcfcb4eae65448f4b10c0b
cindy kwok <kwokc3@rpi.edu>
2022-03-28 20:14:12 -0400

    reformatted profile page with bootstrap

--- mern/campusdate/src/components/profile/profile.component.js
+++ mern/campusdate/src/components/profile/profile.component.js
@@ -1,204 +1,107 @@
+import './listofprofiles.css';
 import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text,  View, Button, Alert, SafeAreaView, TouchableOpacity, Image, Component, ScrollView, Pressable, Dimensions, useState} from 'react-native';
-// import { backgroundColor } from '../../react-native/Libraries/Components/View/ReactNativeStyleAttributes';
-//import {AiFillHeart} from './node_modules/react-icons/ai';
-
-// safe area view might just be ios -.- , porlly just use View
+import { Accordion, ButtonGroup, Container, Figure, Form, ToggleButton, ToggleButtonGroup } from "react-bootstrap";
 
 // TODO: setName function around line 57 won't work
 
 import { Link } from "react-router-dom";
 
-const screenWidth = Dimensions.get('screen').width;
-const screenHeight = Dimensions.get('screen').height;
+export default function ListProfiles(){
+  const hobbies = [ {name:'hobby1', value:1}, {name:'hobby2', value:2}, {name:'hobby3', value:3},{name:'hobby4', value:4}, {name:'hobby5', value:5},{name:'hobby6', value:6},{name:'hobby7', value:7},{name:'hobby8', value:8},{name:'hobby9', value:9}]
 
-const Separator = () => <View style={styles.separator} />;
+  return(
 
-function App() {
-  return (
+    <main>
+      <head>
+     </head>
+      <body>
+        <Container>
+          <ScrollView contentContainerStyle={{
+                        flexGrow: 1,
+                        justifyContent: 'space-between',
+                        flexDirection: 'column',
+                      }}>
+            <Figure.Image className="profileimages-image" style = {{justifyContent: 'flex-start'}} src={require('../assets/anonprofile.png')}/>
+            
+            {/* might want to put name here of person lol */}
 
-    <SafeAreaView style={{backgroundColor:'#FECCC4', width: screenWidth, height: screenHeight }}>
-      
-      {/* <Separator></Separator> */}
+           <Form>
+              <Form.Group className = "hobbiesFormatting">
+              <div class = "row matchedOnPadding"> {/* padding = spacing b/t box and Text */}
+                {hobbies.map((hobby, idx) => (
+                  <div class="col-lg-4 matchedOnPadding"> {/* padding = spacing b/t the form boxes */}
+                    <Form.Control class = "form-control text-center" type="text"  placeholder={hobby.name} disabled></Form.Control>
+                  </div>
+                ))}
+              </div>
+              </Form.Group>
+            </Form> 
 
-      {/* <View style={styles.screen}> */}
+            <Text style = {styles.profileTextSize} className = "profileText">
+I am the fishmonger you're looking for.
 
-        {/* <Image
-              style={styles.profilePic}
-              source={ {uri: 'https://reactnative.dev/img/tiny_logo.png',
-            }} /> */}
+Good day lads!
 
-        <Image
-          style={ styles.profilePic}
-          source={require('../assets/anonprofile.png')} />
+I'm an incredible sort of lady, who likes nothing more than hiking with the right man.
 
-        <View style = {styles.matchedOnButtons1}></View>
-        <View style = {styles.matchedOnButtons2}></View>
-        <View style = {styles.matchedOnButtons3}></View>
-        
-        {/* scrollView wrapped inside another View b/c of scrollView height limitation */}
-        {/* scrollView is off, sometimes it is unresponsive when trying to scroll -.-  */}
-        <View style = {{height: screenHeight/4}}> 
-          <ScrollView
-            style={styles.profileTextScrolling}>
-            <Text
-              style={styles.profileTextSize}>
-              Profile of User: Im writing this as a test to see how it will look depending on how long this is. Also seeing the
-              scroll feature. LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL
-              LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL
-              LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL
-              LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL
-            </Text>
-          </ScrollView> 
-        </View>
+The first thing people usually notice about me is my kind personality, closly followed by my smashing lips. I am not one of those fake people who pretends not to notice their own qualities. My lips and abs are top notch. These gems of honesty are just part of the incredible person I am.
 
-        {/* will prolly be issue w/ diff devices, but did use some hard coded values to get centered images*/}
-        <TouchableOpacity
-          style={styles.roundButtonLeft}
-          /* onPress={() => setName(name+'a')} */ > 
-            <Image source={require('../assets/xIcon.png')} style = {{width: screenWidth/7,height: screenWidth/7, left: 5, top: 5}} />
-        </TouchableOpacity>
+I work as a fishmonger, helping blind people. This allows me to exercise my skills: teaching and bravery. I would like to tell you about the time I met David Bowie, which is true, but it's important to me that you know I'm honest, so I'll save the wilder parts of my life for another time.
 
-        <TouchableOpacity
-          style={styles.roundButtonRight}
-          onPress={() => Alert.alert('Simple Button pressed')}>
-            <Image source={require('../assets/heartIcon.png')} style = {{width: screenWidth/7,height: screenWidth/7,left: 9,top: 8,}} />
-        </TouchableOpacity> 
+My life goals include:
+Star in the next Star Wars film.
+Become the best fishmonger I can be
+Help all the blind people in the world
+If you're the right man for me, you'll be generous and gentle. You won't be afraid to be themselves and will have a healthy respect for perfection.
 
+My ideal date would involve baking in New York with a red man by my side. While we're there, I compliment your solid .
 
-        <View style={{ flexDirection:"row", marginLeft: 0, marginRight:"auto" }}>
-          <Link to="/editprofile">
-          <Pressable
-            style = {styles.menuButtons}
-            onPress={() => Alert.alert('Setting Menu Button')}>
-              <Image source={require('../assets/settingIcon.png')} style = {styles.iconButtons} />
-          </Pressable>
-          </Link>
-          <Pressable
-            style = {styles.menuButtons}
-            onPress={() => Alert.alert('Profile Menu Button')}>
-              <Image source={require('../assets/profileIcon.png')} style = {styles.iconButtons} />
-          </Pressable>
-          <Link to="/chat">
-          <Pressable
-            style = {styles.menuButtons}
-            onPress={() => Alert.alert('Chat Menu Button')}>
-              <Image source={require('../assets/chatIcon.png')} style = {styles.iconButtons} />
-          </Pressable> 
-          </Link>
-        </View>
+Honesty and openness are the most important qualities in a relationship. I will be honest with you, if you will be honest with me. I will never hit on your best friend whilst you're visiting a sick relative, never text my ex behind your back while you're asleep, never post naked photos of you on Facebook. That's just the kind of lady I am.
 
+May the force be with you.
 
-      
+I urge you, get in touch
 
-      {/* </View> */}
-    </SafeAreaView>
+Jane Doe
+          </Text>
+
+          {/* buttons for matching >> left = dont want to match, right = want to match */}
+
+          <div class = "btn-group d-flex" role = "group" aria-label='buttons for matching'>
+            <button type = "button" className = "btn btn-circle"><Figure.Image className = "customNavImage" src={require('../assets/xIcon.png')}></Figure.Image></button>
+            <button type = "button" className = "btn btn-circle2"><Figure.Image className = "customNavImage" src={require('../assets/heartIcon.png')}></Figure.Image></button>
+          </div>
+          
+
+          {/* navigational buttons at bottom of the page w/ links, class has info of aesthetics of buttons */}
+          <div class="btn-group d-flex customNav" role="group" aria-label="navigational buttons">
+              <a href = "/editprofile" button type="button" class="btn btn-secondary w-100 h-100 customNavSize"><Figure.Image className = "customNavImage" src={require('../assets/settingIcon.png')}></Figure.Image></a>
+              <a href = "/profile" button type="button" class="btn btn-secondary w-100 h-100 customNavSize"><Figure.Image className = "customNavImage" src={require('../assets/profileIcon.png')}></Figure.Image></a>
+              <a href = "/chat" button type="button" class="btn btn-secondary w-100 h-100 customNavSize"><Figure.Image className = "customNavImage" src={require('../assets/chatIcon.png')}></Figure.Image></a>
+          </div>
+
+          </ScrollView>
+
+          
+        </Container>
+
+      </body>
+
+       
+    </main>
+
   );
+  
 }
 
 
-/// Just some styles
+
 const styles = StyleSheet.create({
-  roundButtonLeft:{
-    borderRadius: 100,
-    backgroundColor: '#DC6248',
-    width: screenWidth/5.5,
-    height: screenWidth/5.5,
-    left:screenWidth/15,
-    top: screenHeight/3, // increasing brings button down
-    position: 'relative',
+  profileTextSize:{
+    fontSize: 18,
     
-  },
-
-  profilePic: { 
-    width: screenWidth - 6,
-    height: screenHeight/2.3,
-    //marginTop: "-280%", // larger moves to top
-    alignSelf:"center",
-    position: 'absolute',
-    margin: 10,
-    top: 25, 
-  },
-
-  roundButtonRight: {
-    borderRadius: 100,
-    backgroundColor: '#84DC48',
-    width: screenWidth/5.5,
-    height: screenWidth/5.5,
-    left: screenWidth/1.35,
-    top: screenHeight/4, // -.- same thing, using screenHeight but still diff height from left button gah
-    position: 'relative',
-
   }, 
-
-  profileTextScrolling:{
-    width: screenWidth - 30,
-    padding: 10,
-    backgroundColor: "white",
-    borderRadius: 10,
-    top: screenHeight - (screenHeight/1.45),
-    alignSelf:'center',
-    position:'relative',
-    
-  },
-   profileTextSize:{
-    fontSize: 15,
-
-  }, 
-  matchedOnButtons1:{
-    borderWidth: 1,
-    //borderColor: "lightgrey",
-    borderRadius: 100, // rounded edges or no -- higher = more round
-    width: screenWidth/3,
-    height: screenHeight/20,
-    backgroundColor: "white",
-    position: 'relative', 
-    top: screenHeight/2.5,
-    left: 0,
-
-  },
-  matchedOnButtons2:{
-    borderWidth: 1,
-    backgroundColor: "white",
-    //borderColor: "red",
-    borderRadius: 100, 
-    width: screenWidth/3,
-    height: screenHeight/20,
-    position: 'relative', 
-    top: screenHeight/2.85, // confused as to why this isn't just screenHeight/2.5 like before 
-    left: screenWidth/3,
-
-  },
-  matchedOnButtons3:{
-    borderWidth: 1,
-    backgroundColor: "white",
-    //borderColor: "green",
-    borderRadius: 100, 
-    width: screenWidth/3,
-    height: screenHeight/20,
-    position: 'relative', 
-    top: screenHeight/3.30, // same question, why idn't it just og screenHeight/2.5
-    left:(screenWidth/3)*2, // moving 2 "matchedOnButtons" over 
-
-  },
-  menuButtons:{
-    borderColor:'#FECCC4', 
-    borderWidth:1, 
-    backgroundColor:'#FECCC4', 
-    width: screenWidth/3,
-    height: screenHeight/5,
-    top: screenHeight/3.5,
-    position: 'relative',
-
-  },
-iconButtons:{
-  width: screenWidth/6,
-  height: screenWidth/6,
-  left: screenWidth/15,
-}
 
 });
-
-export default App;
-

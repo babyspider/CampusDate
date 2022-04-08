9724dbefa1a6e8575515234e622a01feb7a10e62
lissaa <arianalissak@gmail.com>
2022-04-08 15:58:14 -0400

    fixed multiline commenting

--- mern/server/routes/match.js
+++ mern/server/routes/match.js
@@ -1,8 +1,9 @@
 const express = require("express");
 
-// recordRoutes is an instance of the express router.
-// We use it to define our routes.
-// The router will be added as a middleware and will take control of requests starting with path /record.
+/** recordRoutes is an instance of the express router.
+* We use it to define our routes.
+* The router will be added as a middleware and will take control of requests starting with path /record.
+*/
 const matchRoutes = express();
 
 // This will help us connect to the database

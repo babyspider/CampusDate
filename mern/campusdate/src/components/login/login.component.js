dd319be5baea33077469dcde3d079269900d9b62
cindy kwok <kwokc3@rpi.edu>
2022-04-08 17:16:09 -0400

    undo for CSS

--- mern/campusdate/src/components/login/login.component.js
+++ mern/campusdate/src/components/login/login.component.js
@@ -42,31 +42,75 @@ export default function Login({setToken}) {
   }
 
   return (
-    <View className="background">
-      <Text className="title">CampusDate</Text>
-      <TextInput
-        className="inputbox"
-        keyboardType = "email-address"
-        placeholder="email"
-        autoCorrect = {false}
-        onChange={e => setUserName(e.target.value)}
+    <View style={styles.background}>
+    <Text style={styles.title}>CampusDate</Text>
+    <TextInput
+      style={styles.inputbox}
+      keyboardType = "email-address"
+      placeholder="email"
+      autoCorrect = {false}
+      onChange={e => setUserName(e.target.value)}
+    />
+
+    <TextInput
+      style={styles.inputbox}
+      autoCorrect={false}
+      placeholder="password"
+      secureTextEntry = {true}
+      onChange={e => setPassword(e.target.value)}
       />
 
-      <TextInput
-        className="inputbox"
-        autoCorrect={false}
-        placeholder="password"
-        secureTextEntry = {true}
-        onChange={e => setPassword(e.target.value)}
-        />
-
-      <Link to="/profile">
-      <Pressable
-        className="buttons"
-        onPress={handleSubmit} type = "submit">
-        <Text className="buttonsText">Login</Text>
-      </Pressable>
-      </Link>
-    </View>
+    <Link to="/profile">
+    <Pressable
+      style={styles.buttons}
+      onPress={handleSubmit} type = "submit">
+      <Text style={styles.buttonsText}>Login</Text>
+    </Pressable>
+    </Link>
+  </View>
   );
 }
+
+const styles = StyleSheet.create({
+  inputbox: {
+    backgroundColor: 'white',
+    borderRadius: 5,
+    fontSize: 20,
+    padding: 3,
+    borderColor: 'grey',
+    marginTop: '5%',
+    marginHorizontal: '20%',
+  },
+  buttons: {
+    borderRadius: 20,
+    borderColor: 'grey',
+    marginTop: '5%',
+    marginHorizontal: '20%',
+    textAlign: 'center',
+    borderWidth: 2,
+  },
+  buttonsText: {
+    margin: '1%',
+    textAlign: 'center',
+    color: 'grey',
+    fontSize: 25,
+  },
+  background: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: 'pink',
+    padding: 8,
+  },
+  title: {
+    backgroundColor: 'none',
+    margin: 1,
+    fontSize: 50,
+    color: 'grey',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
+
+Login.propTypes = {
+  setToken: PropTypes.func.isRequired
+};
--- mern/campusdate/src/components/signup/signup.component.js
+++ mern/campusdate/src/components/signup/signup.component.js
@@ -1,4 +1,3 @@
-import './signup.css';
 import * as React from 'react';
 import {
   Text,
@@ -51,37 +50,74 @@ export default function Signup() {
   }
 
   return (
-    <View className="background">
-      <Text className="title">CampusDate</Text>
+    <View style={styles.background}>
+    <Text style={styles.title}>CampusDate</Text>
 
-      <TextInput
-        className="inputbox"
-        placeholder="email"
-        keyboardType = "email-address"
-        //value={this.state.email}
-        onChange={handleEmail}
-        //e=>setEmail(e.target.value)}
-      />
+    <TextInput
+      style={styles.inputbox}
+      placeholder="email"
+      keyboardType = "email-address"
+      //value={this.state.email}
+      onChange={handleEmail}//e=>setEmail(e.target.value)}
+    />
 
-      <TextInput
-         className="inputbox"
-        placeholder="password"
-        //value={this.formData.password}
-        onChange={handlePass}
-      />
+    <TextInput
+      style={styles.inputbox}
+      placeholder="password"
+      //value={this.formData.password}
+    />
 
-      <TextInput
-         className="inputbox"
-        placeholder="confirm password"
-      />
+    <TextInput
+      style={styles.inputbox}
+      placeholder="confirm password"
+    />
 
-      <Link to="/createprofile">
-      <Pressable
-         className="buttons"
-        onPress={submit}>
-        <Text className="buttonsText">Sign Up</Text>
-      </Pressable>
-      </Link>
-    </View>
-  );
+    <Link to="/createprofile">
+    <Pressable
+      style={styles.buttons}
+      onPress={submit}>
+      <Text style={styles.buttonsText}>Sign Up</Text>
+    </Pressable>
+    </Link>
+  </View>
+);
 }
+const styles = StyleSheet.create({
+inputbox: {
+  backgroundColor: 'white',
+  borderRadius: 5,
+  fontSize: 20,
+  padding: 3,
+  borderColor: 'grey',
+  marginTop: '5%',
+  marginHorizontal: '20%',
+},
+buttons: {
+  borderRadius: 20,
+  borderColor: 'grey',
+  marginTop: '5%',
+  marginHorizontal: '20%',
+  textAlign: 'center',
+  borderWidth: 2,
+},
+buttonsText: {
+  margin: '1%',
+  textAlign: 'center',
+  color: 'grey',
+  fontSize: 25,
+},
+background: {
+  flex: 1,
+  justifyContent: 'center',
+  backgroundColor: 'pink',
+  padding: 8,
+},
+title: {
+  backgroundColor: 'none',
+  margin: 1,
+  fontSize: 50,
+  color: 'grey',
+  fontWeight: 'bold',
+  textAlign: 'center',
+},
+});

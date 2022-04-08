c3be50fc177b179185135deb3c22871fd7813d07
lissaa <arianalissak@gmail.com>
2022-04-08 15:30:38 -0400

    fixed spacing to match our style guidelines

--- mern/server/resources/sendMail.js
+++ mern/server/resources/sendMail.js
@@ -2,31 +2,30 @@ const {email_host, email_user, email_pass} = require('./config.json');
 const nodemailer = require("nodemailer");
 
 module.exports = {
-	name: 			'sendMail',
-	description: 	'Given arguments, this should send an email to the desired address via the email listed in config.json',
-	active: 		true,
-	usage: 			`Object.sendMail(recipient,subject,body)`,
-	async execute(recipient, subject, body){
-		var transporter = nodemailer.createTransport({
-			service: `${email_host}`,
-			auth: {
-				user: `${email_user}`,
-				pass: `${email_pass}`
-			}
-		});
-		var mailOptions = {
-			from: `${email_user}`,
-			to: `${recipient}`,
-			subject: `${subject}`,
-			body: `${body}`
-		};
-		transporter.sendMail(mailOptions,function(error,info){
-			if(error){
-				console.log(error);
-			}else{
-				console.log('Email sent: ' + info.response);
-			}
-		});
-
-	}
+    name:           'sendMail',
+    description:    'Given arguments, this should send an email to the desired address via the email listed in config.json',
+    active:         true,
+    usage:          `Object.sendMail(recipient,subject,body)`,
+    async execute(recipient, subject, body){
+        var transporter = nodemailer.createTransport({
+            service: `${email_host}`,
+            auth: {
+                user: `${email_user}`,
+                pass: `${email_pass}`
+            }
+        });
+        var mailOptions = {
+            from: `${email_user}`,
+            to: `${recipient}`,
+            subject: `${subject}`,
+            body: `${body}`
+        };
+        transporter.sendMail(mailOptions,function(error,info){
+            if(error){
+                console.log(error);
+            }else{
+                console.log('Email sent: ' + info.response);
+            }
+        });
+    }
 }

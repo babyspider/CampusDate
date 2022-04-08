cdba2be9ae0f1a37ff965ca92349c1d66fcdbc13
lissaa <arianalissak@gmail.com>
2022-04-08 16:37:31 -0400

    seperated signup css
    also added formatting changes

--- mern/campusdate/src/components/signup/signup.component.js
+++ mern/campusdate/src/components/signup/signup.component.js
@@ -1,3 +1,4 @@
+import './signup.css';
 import * as React from 'react';
 import {
   Text,
@@ -50,11 +51,11 @@ export default function Signup() {
   }
 
   return (
-    <View style={styles.background}>
-      <Text style={styles.title}>CampusDate</Text>
+    <View className="background">
+      <Text className="title">CampusDate</Text>
 
       <TextInput
-        style={styles.inputbox}
+        className="inputbox"
         placeholder="email"
         keyboardType = "email-address"
         //value={this.state.email}
@@ -62,63 +63,24 @@ export default function Signup() {
       />
 
       <TextInput
-        style={styles.inputbox}
+         className="inputbox"
         placeholder="password"
         //value={this.formData.password}
         onChange={handlePass}
       />
 
       <TextInput
-        style={styles.inputbox}
+         className="inputbox"
         placeholder="confirm password"
       />
 
       <Link to="/createprofile">
       <Pressable
-        style={styles.buttons}
+         className="buttons"
         onPress={submit}>
-        <Text style={styles.buttonsText}>Sign Up</Text>
+        <Text className="buttonsText">Sign Up</Text>
       </Pressable>
       </Link>
     </View>
   );
 }
-const styles = StyleSheet.create({
-  inputbox: {
-    backgroundColor: 'white',
-    borderRadius: 5,
-    fontSize: 20,
-    padding: 3,
-    borderColor: 'grey',
-    marginTop: '5%',
-    marginHorizontal: '20%',
-  },
-  buttons: {
-    borderRadius: 20,
-    borderColor: 'grey',
-    marginTop: '5%',
-    marginHorizontal: '20%',
-    textAlign: 'center',
-    borderWidth: 2,
-  },
-  buttonsText: {
-    margin: '1%',
-    textAlign: 'center',
-    color: 'grey',
-    fontSize: 25,
-  },
-  background: {
-    flex: 1,
-    justifyContent: 'center',
-    backgroundColor: 'pink',
-    padding: 8,
-  },
-  title: {
-    backgroundColor: 'none',
-    margin: 1,
-    fontSize: 50,
-    color: 'grey',
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-});
